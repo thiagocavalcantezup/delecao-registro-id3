fb3cbad312aba4c68f0b7f840d7fbfa6243b9e14
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 01:57:37 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/delecaoregistroid3/DelecaoRegistroId3Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.delecaoregistroid3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelecaoRegistroId3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelecaoRegistroId3Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/delecaoregistroid3/DelecaoRegistroId3ApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.delecaoregistroid3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelecaoRegistroId3ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
