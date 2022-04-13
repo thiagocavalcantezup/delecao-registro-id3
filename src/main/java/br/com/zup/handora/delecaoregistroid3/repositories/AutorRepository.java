716877509a3a6ad57037fb29c24b7f6c2bc5928d
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 02:00:24 -0300

    Add Autor repository

---
+++ src/main/java/br/com/zup/handora/delecaoregistroid3/repositories/AutorRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.delecaoregistroid3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.delecaoregistroid3.models.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
